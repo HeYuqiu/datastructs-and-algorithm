5649bbc1f8d6a808a5bdfd8dbebdef628a728598
Yuqiu.He <yuqiu@igola.com>
2020-09-01 10:02:23 +0800

    sort

--- 5-sort/src/main/java/P4QuickSortExamine.java
+++ 5-sort/src/main/java/P4QuickSortExamine.java
@@ -7,21 +7,17 @@
  * @author Yuqiu.He
  * @date 2020-07-01
  */
-public class P4QuickSort {
+public class P4QuickSortExamine {
 
     public static void main(String[] args) {
         int[] a = {4, 2, 5, 1, 6, 3, 4, 7, 8, 9, 11, 3, 23, 8, 10, 25, 11, 2, 19};
-        sort(a);
+        quickSort(a, 0, a.length - 1);
         for (int i : a) {
             System.out.println(i);
         }
     }
 
-    public static void sort(int[] a) {
-        quickSort(a, 0, a.length - 1);
-    }
-
-    private static void quickSort(int[] a, int left, int right) {
+    public static void quickSort(int[] a, int left, int right) {
         if (left < right) {
             int partitionIndex = partition(a, left, right);
             quickSort(a, left, partitionIndex - 1);
@@ -29,25 +25,23 @@ public class P4QuickSort {
         }
     }
 
-    // 获取分区点，重新组合数组：小于分区点放左边，大于分区点放右边(只会影响到left到right这一段数据区间)
-    private static int partition(int[] a, int left, int right) {
-        // 以left作为基准值
+    public static int partition(int[] a, int left, int right) {
         int pivot = left;
         int index = left + 1;
         for (int i = left + 1; i <= right; i++) {
             if (a[i] < a[pivot]) {
-                swap(a, index, i);
+                swap(a, i, index);
                 index++;
             }
         }
-        // 基准值放到中间
-        swap(a, index - 1, pivot);
+        swap(a, pivot, index - 1);
         return index - 1;
     }
 
-    private static void swap(int[] a, int i, int j) {
+    public static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
+
 }
