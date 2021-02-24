d287468da84390e7ab18f833bb1476b4275c0365
Yuqiu.He <yuqiu@igola.com>
2021-02-24 11:30:25 +0800

    动态规划

---
+++ 10-动态规划/src/main/java/DynamicProgramming_最长上升子序列.java
@@ -0,0 +1,26 @@
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *
+ * @author Yuqiu.He
+ * @date 2021/2/8
+ */
+public class DynamicProgramming_最长上升子序列 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i : dp) {
+            i = 1;
+        }
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
